e734a4ba78a88e4e59eb026fec757f3636338f66
tjdgml97 <parksunghee9@naver.co>
2022-07-21 17:10:13 +0900

    edit

--- pages/post/second.js
+++ pages/post/second.js
@@ -15,7 +15,7 @@ export default function FirstPost() {
 
 
 <div><h3>MBTI : </h3>
-<Link href="https://ddnews.co.kr/enfp-%ED%8A%B9%EC%A7%95/"><a><Image className={styles.bo} src="img/enfp.jpg" height={100} width={100}  /> </a></Link>
+<Link href="https://ddnews.co.kr/enfp-%ED%8A%B9%EC%A7%95/"><a><Image className={styles.bo} src="img/enfp.png" height={100} width={100}  /> </a></Link>
 <div> ENFP </div>
 </div>
    <h3>FAVORITE : </h3>
